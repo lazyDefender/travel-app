77257589e816fc36b219cd5726568d4e050b224c
Roman Ortynskyi <50491616+lazyDefender@users.noreply.github.com>
2020-12-06 19:39:03 +0200

    Initial commit

---
+++ src/init/rootReducer.js
@@ -0,0 +1,8 @@
+import {combineReducers} from 'redux';
+
+//import reducers
+
+
+export const rootReducer = combineReducers({
+    // reducers
+});
---
+++ src/pages/home/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Home as HomeComponent} from '../../components/home';
+
+export const Home = (props) => {
+    return <>
+        <HomeComponent/>
+    </>
+}
